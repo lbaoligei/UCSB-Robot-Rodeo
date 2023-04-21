0a2ead47e085ceedeadd98e54988e7325fd6b39e
robotrodeo <clarkengqiu@gmail.com>
2023-04-21 14:39:38 -0700

    Arm node

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'drivetrain'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robotrodeo',
+    maintainer_email='robotrodeo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "controller_node=drivetrain.controller_node:main",
+            "drivetrain_node=drivetrain.drivetrain_node:main",
+            "tail_node=drivetrain.tail_node:main",
+            "arm_node=drivetrain.arm_node:main"
+        ],
+    },
+)
